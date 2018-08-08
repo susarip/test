304fd67e81446c8db72880798ef462c8c639aaca
ssaripalli <susmitha.saripalli@gmail.com>
2018-08-08 18:22:59 -0400

    fixed scraping and added comments

--- hanjie_scraper/hanjie_scraper/spiders/hanjie_spider.py
+++ hanjie_scraper/hanjie_scraper/spiders/hanjie_spider.py
@@ -1,42 +1,52 @@
 
 from scrapy import Spider, Request
 from hanjie_scraper.items import HanjieScraperItem
-
+from scrapy.selector import Selector
 
 
 class HanjieScraper(Spider):
 	name = 'hanjie_scraper'
 	allowed_urls = ['http://www.nonograms.org/nonograms']
-	start_url = ['http://www.nonograms.org/nonograms']
+	start_urls = ['http://www.nonograms.org/nonograms']
 
 
+	# find all pages
 	def parse(self, response):
 		# List comprehension to construct all the urls
-		page_urls = ['http://www.nonograms.org/nonograms/p/'+ str(i) for i in range(1,3)]
-		print(page_urls[0])
+		page_urls = ['http://www.nonograms.org/nonograms/p/'+ str(i) for i in range(1,327)]
 
-		# Yield the requests to different search result urls,
-		# using parse_result_page function to parse the response.
+		# Yield the links to each page on the website
 		for url in page_urls:
-			print("URL:pages:", url)
 			yield Request(url=url, callback=self.parse_pages)
 
+	# Per page parse
 	def parse_pages(self,response):
+		# Get links of each puzzle on the page
 		puzzle_links = response.xpath('//a[@class = "nonogram_title"]/@href').extract()
+		# yield links to each puzzle's page
 		for puzzle in puzzle_links:
 			yield Request(puzzle, callback=self.parse_puzzles)
 
+	# Per puzzle parse
 	def parse_puzzles(self,response):
-		print(response)
+		# Title of just the puzzle without extra text from "Black and white Japanese crossword <<TITLE>>"
 		title = response.xpath('/html/head/title/text()').extract_first()[36:-1]
+		# Get size
 		sizeText = response.xpath('//div[@class = "content"]/table//tr/td[1]/text()').extract_first()
+		# make size into list and assign each dimension to variable
 		size = sizeText[6:].split('x')
 		sizeRow = size[0]
 		sizeCol = size[1]
-		#rowCluesTable = response.xpath('//*[@class = "nmtt"]/table')
-		#colCluesTable = response.xpath('//*[@class = "nmtl"]/table')
+
+		# Need to implement splash to get around JS to get the following fields
+		#rowClues = response.xpath('//*[@class="nmtt"]/table')
+		#colClues = response.xpath('//*[@class="nmtl"]/table')
+
+		# puzzle difficulty, helpful to organize which ones we should start with for neural net
 		difficulty = response.xpath('//div[@class="content"]/table//tr/td[3]/img/@alt').extract_first()
+		# puzzle solution link to image to be used for supervised learning
 		solution = response.xpath('//div[@class = "content"]/a[@class = "lightbox"]/@href').extract_first()
+
 
 		item = HanjieScraperItem()
 		item['title'] = title
