f4c7813a5ce36dba6e8e84bb5995083665f31100
ssaripalli <susmitha.saripalli@gmail.com>
2018-08-07 23:41:19 -0400

    spider edits

--- hanjie_scraper/hanjie_scraper/spiders/hanjie_spider.py
+++ hanjie_scraper/hanjie_scraper/spiders/hanjie_spider.py
@@ -10,14 +10,15 @@ class HanjieScraper(Spider):
 	start_url = ['http://www.nonograms.org/nonograms']
 
 
-
 	def parse(self, response):
 		# List comprehension to construct all the urls
 		page_urls = ['http://www.nonograms.org/nonograms/p/'+ str(i) for i in range(1,3)]
+		print(page_urls[0])
 
 		# Yield the requests to different search result urls,
 		# using parse_result_page function to parse the response.
 		for url in page_urls:
+			print("URL:pages:", url)
 			yield Request(url=url, callback=self.parse_pages)
 
 	def parse_pages(self,response):
@@ -26,16 +27,16 @@ class HanjieScraper(Spider):
 			yield Request(puzzle, callback=self.parse_puzzles)
 
 	def parse_puzzles(self,response):
+		print(response)
 		title = response.xpath('/html/head/title/text()').extract_first()[36:-1]
 		sizeText = response.xpath('//div[@class = "content"]/table//tr/td[1]/text()').extract_first()
 		size = sizeText[6:].split('x')
 		sizeRow = size[0]
 		sizeCol = size[1]
-		#rowClues = response.xpath('').extract_first()
-		#colClues = response.xpath('').extract_first()
+		#rowCluesTable = response.xpath('//*[@class = "nmtt"]/table')
+		#colCluesTable = response.xpath('//*[@class = "nmtl"]/table')
 		difficulty = response.xpath('//div[@class="content"]/table//tr/td[3]/img/@alt').extract_first()
 		solution = response.xpath('//div[@class = "content"]/a[@class = "lightbox"]/@href').extract_first()
-
 
 		item = HanjieScraperItem()
 		item['title'] = title
